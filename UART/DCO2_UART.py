16b16b256aaafa6f90f2bd9d4c29ab676456092e
ina-uzu <ina.uzu.song@gmail.com>
2019-01-23 01:33:38 +0900

    UART for DO/DCO2/PH

---
+++ UART/DCO2_UART.py
@@ -0,0 +1,61 @@
+import sys
+import serial
+import string
+import time
+from serial import SerialException
+
+PORT = "/dev/ttyUSB0"
+BAUD_RATE = 38400
+
+def read_data():
+	buf =[]
+	cnt=0
+	
+	while True :
+		tmp = ser.read(1)
+		print tmp
+		if tmp == " " and cnt<2:
+			cnt = cnt+1
+			continue
+
+		if tmp =="\n":
+			break;
+		buf.append(tmp)
+
+	res =''.join(buf)
+	print("%s" % res)
+
+if __name__ == "__main__":
+
+	print("Opening Serial port")
+	try : 
+		ser = serial.Serial(PORT, BAUD_RATE, timeout=0)
+	except serial.SerialException as e :
+		print("Error : ", e)
+		sys.exit(0)
+	
+	print("1. Read" )
+	print("3. Cal")
+	print("4. Quit")
+
+	# Get command
+	while True :
+		cmd = raw_input("Command : ")
+	
+		if cmd == "Quit" :
+			print("BYE~")
+			break
+			
+		#Reading 
+		elif cmd =="Read" :
+			print("Start Reading ( Stop : Ctrl+C )")
+			while True :
+				try:
+					read_data()
+					time.sleep(1)
+				except KeyboardInterrupt :
+					print("Stop Reading")
+					break
+
+		elif cmd =="Cal" :
+			print ("Calibration done")
---
+++ UART/DO_UART.py
@@ -0,0 +1,79 @@
+import sys
+import serial
+import string
+import time
+from serial import SerialException
+
+PORT = "/dev/serial0"
+BAUD_RATE = 9600
+
+def read_data():
+	buf =[]
+	while True :
+		tmp = ser.read(1)
+		if tmp =="\r":
+			break;
+		buf.append(tmp)
+	res =''.join(buf)
+	print("%s ppm" % res)
+
+def send_cmd(cmd):
+	buf = cmd+'\r'
+	try : 
+		ser.write(buf.encode('utf-8'))
+	except SerialException as e:
+		print("Error : " , e)
+
+def init():
+	send_cmd("R")	
+	while True :
+		tmp = ser.read(1)
+		if tmp =="\r":
+			break;
+	
+if __name__ == "__main__":
+
+	print("Opening Serial port")
+	try : 
+		ser = serial.Serial(PORT, BAUD_RATE, timeout=0)
+	except serial.SerialException as e :
+		print("Error : ", e)
+		sys.exit(0)
+	
+	#Perfom a R cmd to handle the first garbage data
+	init()
+
+	print("1. R" )
+	print("2. C,n")
+	print("3. Cal")
+	print("4. Quit")
+
+	# Get command
+	while True :
+		cmd = raw_input("Command : ")
+	
+		if cmd == "Quit" :
+			print("BYE~")
+			break
+		else :
+			whatCmd = cmd.split(',')
+			send_cmd(cmd)
+
+			# Cont - Reading
+			if whatCmd[0] == "C" :
+				print("Start Reading")
+				while True:
+					try :
+						time.sleep(0.8)
+						read_data()
+					except KeyboardInterrupt :
+						send_cmd("C,0")
+						break
+			
+			# 1 Reading 
+			elif whatCmd[0] =="R" :
+				time.sleep(0.8)
+				read_data()
+
+			elif whatCmd[0]=="Cal" :
+				print ("Calibration done")
